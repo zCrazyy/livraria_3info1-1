fb9ac7af7671e806d5521ed83a9c65a3a0140250
Marco André Mendes <marcoandre@gmail.com>
2022-07-06 14:25:38 -0300

    criação model Livro com fk

--- core/models.py
+++ core/models.py
@@ -27,8 +27,16 @@ class Autor(models.Model):
     class Meta:
         verbose_name_plural = "Autores"
 
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     ISBN = models.CharField(max_length=32)
     quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
+ 
