2f7e5e2c7be621729a6e4ce59c13456b71082eb7
Marco André Mendes <marcoandre@gmail.com>
2022-07-13 15:09:04 -0300

    drf editora

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Categoria
-from core.serializers import CategoriaSerializer
+from core.models import Categoria, Editora
+from core.serializers import CategoriaSerializer, EditoraSerializer
 
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
